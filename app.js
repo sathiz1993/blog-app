f768cfbced5b0a1437fa0c9f8bc1e078d5ffdba6
Sathish Kumar T <sathiz1993@gmail.com>
2018-02-09 11:21:29 +0530

    Added logger middleware

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express')
 var path = require('path')
 var app = express()
+var app = express()
+
+// Logger middleware
+app.use(morgan('combined'))
 
 // Configure middle ware for static
 app.use(express.static(path.join(__dirname, 'public/templates')))
