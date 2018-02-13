ee03e4f1c67d9f37f274fca1aff828beb31d4358
Sathish Kumar T <sathiz1993@gmail.com>
2018-02-13 21:54:19 +0530

    Added user model

---
+++ model/users.js
@@ -0,0 +1,26 @@
+var mangoose = require('mangoose');
+var schema = mangoose.Schema
+
+// Create user schema
+var userSchema = new schema({
+    name: String,
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    age: Number,
+    created_at: Date,
+    updated_at: Date
+});
+
+// Create a model using schema
+var User = mangoose.model('User', userSchema);
+
+// Export to other module
+module.exports = User;
+
