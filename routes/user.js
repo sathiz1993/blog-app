550cccf72bf1b7e9799de539729248c21c7e3a2b
Sathish Kumar T <sathiz1993@gmail.com>
2018-02-14 23:19:17 +0530

    Added user routes for get user,update user and delete user

--- routes/user.js
+++ routes/user.js
@@ -6,4 +6,23 @@ app.post('/users', function(req,res,nxt){
     res.send("Create users");
 });
 
+// Get user
+app.get('/users/:user_id', function(req,res,nxt){
+    var user_id = req.param.user_id;
+    res.send("Get user");
+});
+
+// Update user
+app.patch('/users/:user_id', function(req,res,nxt){
+    var user_id = req.param.user_id;
+    res.send("Update user");
+});
+
+// Get user
+app.delete('/users/:user_id', function(req,res,nxt){
+    var user_id = req.param.user_id;
+    res.send("Delete user");
+});
+
+module.exports = app
 
