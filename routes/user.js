a786fb58d721b9f9d3c3486795421cb06c44773f
Sathish Kumar T <sathiz1993@gmail.com>
2018-02-16 23:28:16 +0530

    Added api for creation and get user

--- routes/user.js
+++ routes/user.js
@@ -1,25 +1,70 @@
 var app = require('../app')
 var logger = require('../config')
+var user = require('../model/users')
+var mangoose = require('mongoose')
 
 // Create users
-app.post('/users', function(req,res,nxt){
-    res.send("Create users");
+app.post('/api/v1/users', function(req,res,nxt){
+    // Get data from request
+    let data = req.body.data;
+
+    // Create user
+    user.create(data, function(err, success){
+        if (err){
+            // Error in creating document
+            let resposne = {
+                'status': 'error',
+                'code': '400',
+                'message': err.errmsg,
+            };
+            res.statusCode = 400;
+            res.send(resposne);
+        }
+        else {
+            // Sucessful creation of document
+            let resposne = {
+                'status': 'success',
+                'code': '200',
+                'message': 'User created successfully',
+                'data': {
+                    'user_id': success._id
+                }
+            };
+            res.json(resposne);
+            };
+        }
+    )
 });
 
 // Get user
-app.get('/users/:user_id', function(req,res,nxt){
-    var user_id = req.param.user_id;
-    res.send("Get user");
+app.get('/api/v1/users/:user_id', function(req,res,nxt){
+    let user_id = req.param.user_id;
+    user.findById(mangoose.Types.ObjectId(user_id), function(err, doc){
+        if (err) {
+            res.status(404).json({
+                'status': 'error',
+                'code': '404',
+                'message': err
+            });
+        } else {
+            res.status(200).json({
+                'status': 'success',
+                'code': '200',
+                'message': 'User find successfully',
+                'data': doc
+            });
+        }
+    })
 });
 
 // Update user
-app.patch('/users/:user_id', function(req,res,nxt){
+app.patch('/api/v1/users/:user_id', function(req,res,nxt){
     var user_id = req.param.user_id;
     res.send("Update user");
 });
 
 // Get user
-app.delete('/users/:user_id', function(req,res,nxt){
+app.delete('/api/v1/users/:user_id', function(req,res,nxt){
     var user_id = req.param.user_id;
     res.send("Delete user");
 });
