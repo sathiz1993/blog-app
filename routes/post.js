87e56cce3de99c1fdda0187cb24f42b524490329
Sathish Kumar T <sathiz1993@gmail.com>
2018-03-04 16:35:37 +0530

    Added api for creating post

--- routes/post.js
+++ routes/post.js
@@ -1,23 +1,45 @@
 var app = require('../app')
 var logger = require('../config')
+var post = require('../model/post')
 
 // Create post
-app.post('/users/:user_id/posts', function(req, res, nxt){
-    res.send("Create post");
+app.post('/users/:user_id/posts', function(req, res, nxt) {
+    // Get the data from request
+    let data = request.body.data;
+
+    // Create post
+    post.create(data, function(err, response) {
+        if (err) {
+            // Error in creating document
+            res.status(400).json({
+                'status': 'error',
+                'code': 400,
+                'message': err.errmsg
+            });
+        } else {
+            // Successfull creation of post
+            res.status(201).json({
+                'status': 'success',
+                'code': 201,
+                'message': 'Successfully posted',
+                'data': response
+            });
+        }
+    });
 });
 
 // Get post
-app.get('/users/:user_id/posts/:post_id', function(req, res, nxt){
+app.get('/users/:user_id/posts/:post_id', function(req, res, nxt) {
     res.send("Get post");
 });
 
 // Update post
-app.patch('/users/:user_id/posts/:post_id', function(req, res, nxt){
+app.patch('/users/:user_id/posts/:post_id', function(req, res, nxt) {
     res.send("Update post");
 });
 
 // Delete post
-app.delete('/users/:user_id/posts/:post_id', function(req, res, nxt){
+app.delete('/users/:user_id/posts/:post_id', function(req, res, nxt) {
     res.send("Delete post");
 });
 
