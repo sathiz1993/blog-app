53877ca10040e990413ea39be8b3829e115fe87e
Sathish Kumar T <sathiz1993@gmail.com>
2018-03-04 16:34:57 +0530

    Added api for updating users

--- routes/user.js
+++ routes/user.js
@@ -4,49 +4,46 @@ var user = require('../model/users')
 var mangoose = require('mongoose')
 
 // Create users
-app.post('/api/v1/users', function(req,res,nxt){
+app.post('/api/v1/users', function(req, res, nxt) {
     // Get data from request
     let data = req.body.data;
 
     // Create user
-    user.create(data, function(err, success){
-        if (err){
+    user.create(data, function(err, success) {
+        if (err) {
             // Error in creating document
-            let resposne = {
+            res.status(400).json({
                 'status': 'error',
                 'code': '400',
                 'message': err.errmsg,
-            };
-            res.statusCode = 400;
-            res.send(resposne);
-        }
-        else {
+            });
+        } else {
             // Sucessful creation of document
-            let resposne = {
+            res.send(201).json({
                 'status': 'success',
                 'code': '200',
                 'message': 'User created successfully',
                 'data': {
                     'user_id': success._id
                 }
-            };
-            res.json(resposne);
-            };
+            });
         }
-    )
+    })
 });
 
 // Get user
-app.get('/api/v1/users/:user_id', function(req,res,nxt){
+app.get('/api/v1/users/:user_id', function(req, res, nxt) {
     let user_id = req.param.user_id;
-    user.findById(mangoose.Types.ObjectId(user_id), function(err, doc){
+    user.findById(mangoose.Types.ObjectId(user_id), function(err, doc) {
         if (err) {
+            // Error response
             res.status(404).json({
                 'status': 'error',
                 'code': '404',
                 'message': err
             });
         } else {
+            // Success response
             res.status(200).json({
                 'status': 'success',
                 'code': '200',
@@ -58,16 +55,32 @@ app.get('/api/v1/users/:user_id', function(req,res,nxt){
 });
 
 // Update user
-app.patch('/api/v1/users/:user_id', function(req,res,nxt){
-    var user_id = req.param.user_id;
-    res.send("Update user");
+app.patch('/api/v1/users/:user_id', function(req, res, nxt) {
+    let user_id = req.param.user_id;
+    user.findById(mangoose.Types.ObjectId(user_id), function(err, doc){
+        if (err) {
+            res.status(400).json({
+                // Error response
+                'status': 'error',
+                'code': 400,
+                'message': err
+            })
+        }
+        else {
+            // Success response
+            res.status(200).json({
+                'status': 'success',
+                'code': 200,
+                'message': 'User updated successfully'
+            })
+        }
+    })
 });
 
 // Get user
-app.delete('/api/v1/users/:user_id', function(req,res,nxt){
+app.delete('/api/v1/users/:user_id', function(req, res, nxt) {
     var user_id = req.param.user_id;
     res.send("Delete user");
 });
 
 module.exports = app
-
