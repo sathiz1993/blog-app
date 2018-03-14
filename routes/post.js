07a9a96f8bd019d3cfc8602ca7046df13d7a5add
Sathish Kumar T <sathiz1993@gmail.com>
2018-03-14 23:16:06 +0530

    Added api for get individual post

--- routes/post.js
+++ routes/post.js
@@ -30,7 +30,27 @@ app.post('/users/:user_id/posts', function(req, res, nxt) {
 
 // Get post
 app.get('/users/:user_id/posts/:post_id', function(req, res, nxt) {
-    res.send("Get post");
+    // Get the post id
+    let post = req.param.post_id;
+
+    // Get the post
+    post.findById(mangoose.Types.ObjectId(post_id), function(err, data){
+        if (err) {
+            // Return the error resposne
+            res.status(500).json({
+                'status': 'error',
+                'code': 500,
+                'message': 'Unable to get the data'
+            })
+        } else {
+            // Return the post data
+            res.status(200).json({
+                'status': 'success',
+                'code': 200,
+                'data': data
+            })
+        }
+    })
 });
 
 // Update post
