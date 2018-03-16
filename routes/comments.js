3b300e816b514c7ad5696c8d1e61d2fb551838e2
Sathish Kumar T <sathiz1993@gmail.com>
2018-03-16 23:05:55 +0530

    Add functionality for getting all the comments of post

--- routes/comments.js
+++ routes/comments.js
@@ -1,14 +1,66 @@
 var app = require('../app')
 var logger = require('../config')
+var comment = require('../model/comment')
 
 // Create comment
 app.post('/posts/:post_id/comments', function(req,res,nxt){
-    res.send("add post");
+	// Get the data from request
+	let data = request.body.data;
+
+	// Get the post id
+	let post_id = req.param.post_id
+
+	// Embeded post doc and create
+
+	// Create the comment
+	comment.create(data, function(err, response){
+		if (err){
+			// Error in creating post
+			res.status(500).json({
+				'status': 'error',
+				'code': 500,
+				'message': err.errmsg
+			});
+		} else {
+			// Comment creation success
+			res.status(201).json({
+				'status': 'success',
+				'code': 201,
+				'message': 'Comment creation success.'
+			});
+		}
+	})
 });
 
 // Get all comment of a post
 app.get('/posts/:post_id/comments', function(req, res, nxt){
-    res.send("Get all comment");
+    // Get the post id
+	let post_id = req.param.post_id
+
+	// Query for getting comments of the post
+	query = {
+		'post_id': post_id
+	}
+
+	comment.find(query).limit(10).sort({'created': -1}).exec(function(err, docs){
+		if (comments) {
+			// Return the response with filtered results
+			res.json(200).json({
+				'status': 'success',
+				'code': 200,
+				'data': docs.map(doc:function(doc){
+					return doc.toResJSON();
+				});
+			});
+		} else {
+			// Rerurn the error response
+			res.json(400).json({
+				'status': 'error',
+				'code': 500,
+				'message': 'Unable to get the data'
+			});
+		}
+	});
 });
 
 // Get single comment
